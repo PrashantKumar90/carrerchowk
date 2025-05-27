f58a9ebf96739f592e570ae9bb950e6e1f87d723
PrashantKumar90 <prashantkumarstm2004@gmail.com>
2025-05-28 00:50:52 +0530

    Vercel json update in frontend

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -22,7 +22,7 @@ const ContactForm = () => {
     setSuccess(false);
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://carrerchowk-backend.vercel.app/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
